f82813fef483664d396477d53f936ad0c5a36943
MatasVaitkevicius <vaitkevicius.matas@gmail.com>
2021-12-05 15:09:01 +0200

    Login form styling

--- src/Login.js
+++ src/Login.js
@@ -7,79 +7,86 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import LoadingButton from "@mui/lab/LoadingButton";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
 
 function Login({ dispatch }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
-    console.log("handlesubmit");
-    e.preventDefault();
-    dispatch({
-      type: POST_DATA,
-      payload: {
-        email: email,
-        password: password,
-        apiUrl: "http://localhost:98/api/login",
-      },
-    });
-  };
+    const handleSubmit = (e) => {
+        console.log("handlesubmit");
+        e.preventDefault();
+        dispatch({
+            type: POST_DATA,
+            payload: {
+                email: email,
+                password: password,
+                apiUrl: "http://localhost:98/api/login",
+            },
+        });
+    };
 
-  const handleChange = (e) => {
-    e.target.id === "email"
-      ? setEmail(e.target.value)
-      : setPassword(e.target.value);
-  };
+    const handleChange = (e) => {
+        e.target.id === "email"
+            ? setEmail(e.target.value)
+            : setPassword(e.target.value);
+    };
 
-  return (
-    <>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField required label="E-mail" variant="outlined" />
-        <TextField
-          required
-          label="Password"
-          type="password"
-          variant="outlined"
-        />
-        <LoadingButton
-          // onClick={handleClick}
-          loading={loading}
-          loadingIndicator="Loading..."
-          variant="outlined"
-        >
-          Fetch data
-        </LoadingButton>
-      </Box>
-      <form onSubmit={handleSubmit}>
-        <label for="email">E-mail:</label>
-        <br />
-        <input id="email" type="text" value={email} onChange={handleChange} />
-        <br />
-        <label for="password">Password:</label>
-        <br />
-        <input
-          id="password"
-          type="text"
-          value={password}
-          onChange={handleChange}
-        />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-    </>
-  );
+    return (
+        <>
+            <Container component="main" maxWidth="xs">
+                <Grid container
+                    alignItems="center"
+                    justifyContent="center"
+                    style={{ minHeight: '100vh' }}
+                >
+                    <Box
+                        component="form"
+                        sx={{
+                            "& > :not(style)": { m: 1, width: "25ch" },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <TextField required label="E-mail" variant="outlined"
+                            required
+                            id="email"
+                            label="email"
+                            type="email"
+                            variant="outlined"
+                            value={email}
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            required
+                            id="password"
+                            label="Password"
+                            type="password"
+                            variant="outlined"
+                            value={password}
+                            onChange={handleChange}
+                        />
+                        <LoadingButton
+                            onClick={handleSubmit}
+                            // loading={loading}
+                            loadingIndicator="Loading..."
+                            variant="outlined"
+                        >
+                            Login
+                        </LoadingButton>
+                    </Box>
+                </Grid>
+            </Container>
+        </>
+    );
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  return { dispatch };
+    return { dispatch };
 };
 
 // const mapStateToProps = (store) => {
